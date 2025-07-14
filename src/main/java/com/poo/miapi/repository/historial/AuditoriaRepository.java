4a6c8e9949802d18547f5d3506aee3613a927bf4
Sebastian Conde <sebastianconde60@gmail.com>
2025-07-14 19:43:53 -0300

    Auditoria

--- src/main/java/com/poo/miapi/repository/historial/AuditoriaRepository.java
+++ src/main/java/com/poo/miapi/repository/historial/AuditoriaRepository.java
@@ -1,5 +1,20 @@
 package com.poo.miapi.repository.historial;
 
-public class AuditoriaRepository {
+import com.poo.miapi.model.historial.Auditoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface AuditoriaRepository extends JpaRepository<Auditoria, Long> {
+
+    List<Auditoria> findByUsuario(String usuario);
+
+    List<Auditoria> findByEntidad(String entidad);
+
+    List<Auditoria> findByAccion(String accion);
+
+    List<Auditoria> findByFechaBetween(LocalDateTime startDate, LocalDateTime endDate);
 }
